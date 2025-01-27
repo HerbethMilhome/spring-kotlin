3cd254f73ab40edfcf96719498804950f61c6d1a
Herbeth Milhome <h2milhome@gmail.com>
2025-01-27 19:14:00 -0300

    create migration

---
+++ spring-kotlin/src/main/resources/db/migration/V2__Populate_table_person.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO `person` VALUES (1,'Aqui em casa','Herbeth','Male','Milhome'),(3,'Aqui mesmo','Herbeth','Male','Helder'),(4,'Aqui mesmo','Joana','Female','Dark tesuda'),(6,'Aqui mesmo','Ivar','M','Milhome'),(8,'Aqui mesmo','Ayla','M','Milhome'),(9,'Aqui mesmo','Ayla Dark','M','Milhome');
